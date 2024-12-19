e70ea237d2366618e89ffe5f571f4b6a374fa501
Giuseppe <galardigiuseppe11@gmail.com>
2024-12-20 00:20:34 +0100

    new tests added

--- src/test.cpp
+++ src/test.cpp
@@ -1,3 +1,4 @@
+#include <execution>
 #include <iostream>
 #include <vector>
 #include <complex>
@@ -36,15 +37,43 @@ verify(
         if(abs(standard[i]-sequence[i])>1e-7){
             cout << "Error in " << name << " at index " << i << endl;
             cout << "Expected: " << standard[i] << " Got: " << sequence[i] << endl;
+            return;
         }
     }
     cout << "Test passed for " << name << endl;
 }
 
-int main() {
-    // 2^20 elements
+void test_speed_correctness_fft() {
+    // 2^20
+    constexpr size_t max_dim = 1048576;
+    for(size_t dim = 4; dim <= max_dim; dim*=2) {
+        vector<complex<double>> sequence(dim);
+
+        for(size_t i=0; i<dim; ++i){
+            double rand_real =
+                static_cast<double>(rand()) / static_cast<double>(RAND_MAX);
+            double rand_i =
+                static_cast<double>(rand()) / static_cast<double>(RAND_MAX);
+            sequence[i] = complex<double>(rand_real,rand_i);
+        }
+
+        cout << "[TEST dim=" << dim<< "]" << endl;
+        auto fft_ref = time_function(fft_radix2, sequence, "normal");
+        auto fft = time_function(fft_radix2_parallel, sequence, "parallel");
+        verify(fft_ref, fft, "parallel");
+        fft = time_function(fft_radix2_lookup, sequence, "lookup");
+        verify(fft_ref, fft, "lookup");
+        fft = time_function(fft_radix2_lookup_parallel, sequence, "lookup p");
+        verify(fft_ref, fft, "lookup parallel");
+        
+        cout << "\n\n"; 
+    }
+}
+
+void test_fft_ifft() {
     constexpr size_t dim = 1048576;
     vector<complex<double>> sequence(dim);
+
     for(size_t i=0; i<dim; ++i){
         double rand_real =
             static_cast<double>(rand()) / static_cast<double>(RAND_MAX);
@@ -52,13 +81,59 @@ int main() {
             static_cast<double>(rand()) / static_cast<double>(RAND_MAX);
         sequence[i] = complex<double>(rand_real,rand_i);
     }
-    auto fft_ref = time_function(fft_radix2, sequence, "normal");
-    auto fft = time_function(fft_radix2_parallel, sequence, "parallel");
-    verify(fft_ref, fft, "parallel");
-    fft = time_function(fft_radix2_lookup, sequence, "lookup");
-    verify(fft_ref, fft, "lookup");
-    fft = time_function(fft_radix2_lookup_parallel, sequence, "lookup p");
-    verify(fft_ref, fft, "lookup parallel");
+
+
+    vector<complex<double>> ifft(dim);
+    ifft_radix2(sequence, ifft);
+    vector<complex<double>> output(dim);
+    fft_radix2(ifft, output);
+
+    for(size_t i = 0; i < output.size(); ++i) {
+        if(abs(output[i]-sequence[i]) > 1.0e-7 ) {
+            cout << "Error in test_ifft at index " << i << endl;
+            return;
+        }
+    }
+
+    cout << "Test test_fft_ifft passed" << endl;
+}
+
+void test_ifft_fft() {
+    constexpr size_t dim = 1048576;
+    vector<complex<double>> sequence(dim);
+
+    for(size_t i=0; i<dim; ++i){
+        double rand_real =
+            static_cast<double>(rand()) / static_cast<double>(RAND_MAX);
+        double rand_i =
+            static_cast<double>(rand()) / static_cast<double>(RAND_MAX);
+        sequence[i] = complex<double>(rand_real,rand_i);
+    }
+
+    vector<complex<double>> fft(dim);
+    fft_radix2(sequence, fft);
+    vector<complex<double>> output(dim);
+    ifft_radix2(fft, output);
+
+    for(size_t i = 0; i < output.size(); ++i) {
+        if(abs(output[i]-sequence[i]) > 1.0e-7 ) {
+            cout << "Error in test_ifft at index " << i << endl;
+            return;
+        }
+    }
+
+    cout << "Test test_ifft_fft passed" << endl;
+}
+
+int main() {
+    // Speed and correctness test
+    test_speed_correctness_fft();
+
+    // fft(ifft(sequence)) must be equal to sequence
+    test_fft_ifft();
+
+    // ifft(fft(sequence)) must be equal to sequence
+    test_ifft_fft();
 
     return 0;
 }
